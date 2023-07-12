c77525e7c86f4835f59c5fd6f6c181db4f56b0e0
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-07-12 15:12:45 +0300

    design

--- src/App.jsx
+++ src/App.jsx
@@ -27,32 +27,30 @@ function App() {
   };
 
   // поиск координат пользователя
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords);
-      console.log(navigator.geolocation);
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    });
-  }, []);
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     console.log(position.coords);
+  //     console.log(navigator.geolocation);
+  //     setLatitude(position.coords.latitude);
+  //     setLongitude(position.coords.longitude);
+  //   });
+  // }, []);
 
   // добавление города
   const addCity = (userInput) => {
-    if(userInput) {
+    if (userInput) {
       const newItem = {
-        id: Math.random().toString(36).substring(2,9),
-        task:userInput
-      }
-      setTodos([...todos, newItem])
+        id: Math.random().toString(36).substring(2, 9),
+        task: userInput,
+      };
+      setTodos([...todos, newItem]);
     }
-  }
+  };
 
-    // удаление города
+  // удаление города
   const removeCity = (id) => {
-    setTodos([...todos.filter((todo) => todo.id !== id)])
-  }
-
-
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
 
   return (
     <>
@@ -63,14 +61,8 @@ function App() {
             onChange={(event) => setLocation(event.target.value)}
             onKeyDown={searchLocation}
             placeholder="Enter Location"
-            type="search"
+            type="text"
           />
-          {/* <button
-            onChange={(event) => setLocation(event.target.value)}
-            onClick={searchLocation}
-          >
-            Поиск погоды
-          </button> */}
         </div>
         <div className="container">
           <div className="top">
@@ -78,12 +70,37 @@ function App() {
               <p>{data.name}</p>
             </div>
             <div className="temp">
-              {data.weather ? <p>{data.main.temp.toFixed()}</p> : null}
+              {data.weather ? <h1>{data.main.temp.toFixed()} ℃</h1> : null}
             </div>
-            <div className="position"></div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
           </div>
-        </div>
 
+          {data.name != undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">{data.main.feels_like} ℃</p>
+                ) : null}
+                <p>Feels like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">{data.wind.speed} km/h</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
+{/* 
         <SaveCityForm addCity={addCity} />
         {todos.map((todo) => {
           return (
@@ -93,7 +110,7 @@ function App() {
             key={todo.id}
             />
           )
-        })}
+        })} */}
       </div>
     </>
   );
